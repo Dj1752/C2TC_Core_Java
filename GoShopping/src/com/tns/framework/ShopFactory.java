359d88d7a12cef3529f985752be62f3f2153e315
Dj1752 <83537437+Dj1752@users.noreply.github.com>
2022-08-12 21:23:15 +0530

    Delete GoShopping directory

--- GoShopping/src/com/tns/framework/ShopFactory.java
+++
@@ -1,7 +0,0 @@
-package com.tns.framework;
-
-public abstract class ShopFactory {
-  public abstract PrimeAcc getNewPrimeAcc(int AccNo, String accNm, float charges, boolean isPrime);
-  public abstract NormalAcc getNewNormalAcc(int AccNo, String accNm, float charges, float deliveryCharges);
-  
-}
